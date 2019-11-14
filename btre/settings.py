d40883f0188c639454e090cccda70763afebf416
marie-reine-n <57495661+marie-reine-n@users.noreply.github.com>
2019-11-14 10:09:14 +0100

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -150,5 +150,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nimreine@gmail.com'
-EMAIL_HOST_PASSWORD = 'ngulukulu123'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
